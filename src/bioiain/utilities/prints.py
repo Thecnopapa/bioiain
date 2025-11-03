c45ac138d54fff54261cb9c1104ec0a52b7a600a
Iain <iainvisa@gmail.com>
2025-11-03 14:10:18 +0100

    Refactored logging system

--- src/bioiain/utilities/prints.py
+++ src/bioiain/utilities/prints.py
@@ -1,50 +1,73 @@
 
-import sys, os, shutil
+import os, shutil
+import warnings
+
+
+def log(level:int|str=1, *args, **kwargs):
+    """
+    Log a message and display it according to the given level if higher than environment variable "BI_VERBOSE".
+    Builtin prints are always displayed.
+    If unset BI_VERBOSE is set to 10.
+    BI_VERBOSE == 0 displays only ERROR, WARNING and DEBUG messages.
+    BI_VERBOSE == -1 display only ERROR.
+    BI_VERBOSE == -1 display nothing.
+    :param level: Verbose level: ERROR | WARNING | DEBUG | TITLE | HEADER | int
+    :param args: args for print function
+    :param kwargs: kwargs for print function
+    """
+    v = int(os.environ.get("BI_VERBOSE", 10))
+    if type(level) is str:
+        level = level.lower()
+    if v > -2:
+        if level == "error":
+
+            if isinstance(kwargs.get("error", None), Exception):
+                raise kwargs.get("error")
+            else:
+                print("\033[91m")
+                print("ERROR: ", end="")
+                print(*args, **kwargs)
+                print("\033[0m")
+        elif v > -1:
+            if level == "warning":
+                print("\033[93m")
+                print("WARNING: ", end="")
+                print(*args, **kwargs)
+                print("\033[0m")
+            elif level == "debug":
+                print(*args, **kwargs)
+            elif v > 0:
+                if level == 0 or level is None:
+                    print(*args, **kwargs)
+                elif level == "title":
+                    tprint(*args, **kwargs)
+                elif level == "header":
+                    sprint(*args, **kwargs)
+                elif type(level) is int:
+                    if v >= level:
+                        print1(*args, space=2*level, **kwargs)
+                else:
+                    print("Unknown log level: {}".format(level))
 
 
 
-
-def print(*args, **kwargs):
-    try:
-        if os.environ["BI_VERBOSE"]:
-            vars.log(*args, **kwargs)
-    except:
-        pass
-    import builtins
-    builtins.print(*args, **kwargs)
-
-
-def tprint(*strings, head=10, style="#", end="\n", sep=" ", log=True):  # Print section title
+def tprint(*strings:str, head:int=10, style:str="#", end:str="\n", sep:str=" "):  # Print section title
     width = shutil.get_terminal_size()[0] -2
     string = " ".join(strings)
     tail = width - head - len(string)
     out = "\n{}{}{}{}{}".format(style*head, sep, string, sep, style*tail)
-    try:
-        if not vars.verbose:
-            vars.log(out, end = end, timestamp=False)
-    except:
-        pass
     print(out, end=end)
 
 def eprint(*strings, head=10, style = "^", sep=" "):  # Print end of section
     tprint(*strings, head=head, style=style, end="\n\n", sep=sep)
 
-def sprint(*strings,**kwargs): # Print Subtitle
+def sprint(*strings:str, **kwargs): # Print Subtitle
     str_strings = map(str, strings)
     prefix = "\n"
     out = " # "+ " ".join(str_strings)
-    try:
-        if not vars.verbose:
-            vars.log(out,prefix = prefix, **kwargs)
-    except:
-            pass
-    if globals_loaded and "quiet" in vars:
-        if vars.quiet:
-            return
-
     print(prefix+out,**kwargs)
 
-def print1(*strings, space=2, log=True, **kwargs): # Print with 1 indent
+def print1(*strings:str, space:int=2, **kwargs): # Print with 1 indent
     str_strings = []
     for string in strings:
         if type(string) == list or type(string) == tuple:
@@ -54,27 +77,5 @@ def print1(*strings, space=2, log=True, **kwargs): # Print with 1 indent
             str_strings.append(str(string))
     #str_strings = map(str, strings)
     out = "{}> {}".format(" " * space, " ".join(str_strings))
-    try:
-        if not vars.verbose:
-            vars.log(out, **kwargs)
-    except:
-        pass
-    if globals_loaded:
-        if vars.quiet:
-            return
     print(out, **kwargs)
 
-def print2(*strings, **kwargs): # Print with 2 indents
-    print1(strings, space=4, **kwargs)
-
-def print3(*strings, **kwargs):
-    print1(strings, space=6, **kwargs)
-
-def print4(*strings, **kwargs):
-    print1(strings, space=8, **kwargs)
-
-def print5(*strings, **kwargs):
-    print1(strings, space=10, **kwargs)
-
-def print6(*strings, **kwargs):
-    print1(strings, space=12, **kwargs)
