114bcc750deff81c32878d1a02b7fc74961b012c
Iain@obsidian <iainvisa@gmail.com>
2026-02-23 16:53:11 +0100

    MIX fix

--- test/test.py
+++ test/test.py
@@ -277,7 +277,7 @@ if "-t" in sys.argv:
         epochs = int(sys.argv[sys.argv.index("--epochs") + 1])
 
     
-    run_name = f"{data_name}_LR{str(LR).split(".")[-1]}_E{epochs}"
+    run_name = f"{data_name}_LR{str(LR).split(".")[-1]}_E{epochs}_MIX:{int(MIX)}"
     model = model_class(name=run_name, in_shape=(1280,), num_classes=len(label_to_index), lr=LR, weights=label_count  ).to(DEVICE)
     model.add_map(dataset)
     run_name = model.data["name"]
@@ -294,9 +294,10 @@ if "-t" in sys.argv:
         epoch = epoch +1
 
         log("header", "EPOCH:", epoch)
-        dataset.split()
         if MIX:
-            dataset.train()
+            dataset.split()
+
+        dataset.train()
 
 
         if "no-dropout" in model.layers.keys() and epoch > 10:
