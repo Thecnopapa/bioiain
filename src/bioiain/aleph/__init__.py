dd77638be497824f4e96d74071b5b5e536e2a76d
Iain <iainvisa@gmail.com>
2026-05-26 09:02:54 +0200

    import fix

--- src/bioiain/aleph/__init__.py
+++ src/bioiain/aleph/__init__.py
@@ -3,5 +3,7 @@
 from .vectors import *
 from .core import *
 from .fragments import *
+from .ALEPH2 import *
 
-__all__ = ["vectors", "fragments", "core"]
+
+__all__ = ["vectors", "fragments", "core", "FragmentedStructure", "CVector", "CVMatrix", "CVPair", "ALEPH2"]
