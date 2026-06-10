1b97dd7b443580e233dd29310f3d9f97ca5bc5f9
iainv <iainvisa@gmail.com>
2026-06-10 15:51:40 +0200

    server paths

--- wiki/server/app/server.py
+++ wiki/server/app/server.py
@@ -1,10 +1,13 @@
 import flask, requests, os, sys, json
 from werkzeug.utils import secure_filename
+from werkzeug.exceptions import NotFound
 
 app = flask.Flask(__name__)
 app.secret_key = os.environ['FLASK_KEY']
-
-version_list=sorted(os.listdir("/docs"), reverse=True)
+try:
+    version_list=sorted(os.listdir("/docs"), reverse=True)
+except FileNotFoundError:
+    version_list = ["0.2.0"]
 latest_version=version_list[0]
 print("Version list: {}".format(version_list))
 print("Latest version: {}".format(latest_version))
@@ -13,25 +16,43 @@ print("Latest version: {}".format(latest_version))
 def index():
     return flask.redirect(f"/{latest_version}/index.html")
 
+@app.route('/<version>/')
+def redirect_version_to_index(version):
+    return flask.redirect(f"/{version}/index.html")
+
+@app.route('/<version>/<path:path>/')
+def redirect_to_index(version, path):
+    return flask.redirect(f"/{version}/{path}/index.html")
+
+@app.route('/<version>/<filename>')
 @app.route('/<version>/<path:path>/<filename>')
 def page(version=None, path=None, filename=None):
+    version = version or latest_version
+    version = secure_filename(version)
     print(version)
     if version not in version_list:
+        print(f"Version ({version})not found")
         return flask.abort(404)
     filepath = f"/docs/{version}"
-    path = secure_filename(path)
+
     print(path)
     if path is None or path == '':
         pass
     else:
+        path = secure_filename(path)
         filepath = os.path.join(filepath, path)
 
     if filename is None or filename == '':
         filename = "index.html"
-    version = version or latest_version
+
     print(version, filepath, filename)
 
-    return flask.send_from_directory(filepath, filename)
+    try:
+        resp = flask.send_from_directory(filepath, filename)
+    except NotFound:
+        print(f"File ({filepath}/{filename}) not found")
+        return flask.abort(404)
+    return resp
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
