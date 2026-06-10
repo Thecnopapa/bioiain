7a464a565ab4c076a5ddafb07566bf061dd35c4e
iain@lavander <iainvisa@gmail.com>
2026-06-10 10:16:02 +0000

    removed deprecated symmeteies submodule

--- src/bioiain/symmetries/__init__.py
+++
@@ -1,8 +0,0 @@
-__all__ = ["operations", "parsing", "elements", "crystal", "oligomer", "interactions"]
-
-from .operations import *
-from .parsing import *
-from .elements import Monomer, Ligand
-from .crystal import Crystal
-from .interactions import *
-#from .oligomer import Oligomer
