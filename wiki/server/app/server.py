f49bd01a90e7be2fd03a3cd968ab334f785e1ff9
iainv <iainvisa@gmail.com>
2026-06-10 18:11:25 +0200

    add torch and aleph to docs

--- wiki/server/app/server.py
+++ wiki/server/app/server.py
@@ -24,7 +24,7 @@ def index():
 @app.route('/<version>/<path:path>/')
 @app.route('/<version>/<filename>')
 @app.route('/<version>/<path:path>/<filename>')
-def debugpage(version=None, path=None, filename=None):
+def page(version=None, path=None, filename=None):
     print("Fetching page...")
     if version is None:
         version = latest_version
@@ -61,38 +61,6 @@ def debugpage(version=None, path=None, filename=None):
 
     return resp
 
-def page(version=None, path=None, filename=None):
-    print("Fetching page...")
-    if version is None:
-        version = latest_version
-    else:
-        version = secure_filename(version)
-    print(version)
-    if version not in version_list:
-        print(f"Version ({version})not found")
-        return flask.abort(404)
-    filepath = f"/docs/{version}"
-
-    print(path)
-    if path is None or path == '':
-        pass
-    else:
-        path = secure_filename(path)
-        filepath = os.path.join(filepath, path)
-
-    if filename is None or filename == '':
-        filename = "index.html"
-
-    filepath = os.path.join(filepath, filename)
-
-    print(filepath)
-    if not os.path.exists(filepath):
-        print(f"File ({filepath}/{filename}) not found")
-        return flask.abort(404)
-
-    with open(filepath) as f:
-        resp = flask.make_response(f.read())
-    return resp
 
 
 if __name__ == "__main__":
