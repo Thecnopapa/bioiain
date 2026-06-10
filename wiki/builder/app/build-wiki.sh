60a15d1298f561c080fb1b14419bd4ad21b8a9f6
iainv <iainvisa@gmail.com>
2026-06-10 13:36:30 +0200

    build-wiki.sh

--- wiki/builder/app/build-wiki.sh
+++ wiki/builder/app/build-wiki.sh
@@ -4,10 +4,15 @@ VIRTUAL_ENV="./venv"
 PATH="./venv/bin:$PATH"
 which python
 pwd
+ls
 
+echo "$(pip3 show bioiain)"
+echo "$(pip3 show bioiain | grep Version)"
 
 BIOIAIN_VERSION=$(pip3 show bioiain | grep Version)
+echo $BIOIAIN_VERSION
 BIOIAIN_VERSION=($BIOIAIN_VERSION)
+echo $BIOIAIN_VERSION
 BIOIAIN_VERSION=${BIOIAIN_VERSION[1]}
 echo "BIOIAIN_VERSION=${BIOIAIN_VERSION}"
 
