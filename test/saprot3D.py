89aa7331583eac1b236fa6d3d07a81b9ffc5b649
Iain <iainvisa@gmail.com>
2026-07-24 12:09:11 +0200

    fig fix

--- test/saprot3D.py
+++ test/saprot3D.py
@@ -660,7 +660,7 @@ if INFERENCE:
                 voxels3d(diff_diff, ax = diff_diff_ax, shrink = True, title="Diff diff")
 
 
-                abs_out_ax = fig.add_subplot(n_rows, n_figs+1, 5, projection="3d")
+                abs_out_ax = fig.add_subplot(n_rows, n_figs, n_figs+5, projection="3d")
                 abs_label_x_detached = abs_label_x.detach().cpu().numpy()[0]
                 abs_out = abs_label_x_detached + scaled_out
                 voxels3d(abs_out, ax = abs_out_ax, shrink = True, title="abs + out")
