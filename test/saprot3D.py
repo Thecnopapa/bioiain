24ffe70b68ed5ab8913ecf60181a99c5f3e33695
Iain <iainvisa@gmail.com>
2026-07-24 12:06:15 +0200

    rel - abs + out

--- test/saprot3D.py
+++ test/saprot3D.py
@@ -660,10 +660,15 @@ if INFERENCE:
                 voxels3d(diff_diff, ax = diff_diff_ax, shrink = True, title="Diff diff")
 
 
-                abs_out_ax = fig.add_subplot(n_rows, n_figs, 5, projection="3d")
+                abs_out_ax = fig.add_subplot(n_rows, n_figs+1, 5, projection="3d")
                 abs_label_x_detached = abs_label_x.detach().cpu().numpy()[0]
-                abs_out = abs_label_x_detached - scaled_out
+                abs_out = abs_label_x_detached + scaled_out
                 voxels3d(abs_out, ax = abs_out_ax, shrink = True, title="abs + out")
+
+                rel_out_ax = fig.add_subplot(n_rows, n_figs, 5, projection="3d")
+                rel_label_x_detached = rel_label_x.detach().cpu().numpy()[0]
+                rel_out = rel_label_x_detached - abs_out
+                voxels3d(rel_out, ax = rel_out_ax, shrink = True, title="rel - (abs + out)")
 
 
 
